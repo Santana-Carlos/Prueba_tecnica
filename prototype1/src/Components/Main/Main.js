df8103fbcb1f0748009826c629f645d8c5599cd0
Diego Colmenares <diego.colmenares@uao.edu.co>
2021-05-04 23:14:31 -0500

    commit 03
    basic logic, calculating top 3

--- prototype1/src/Components/Main/Main.js
+++ prototype1/src/Components/Main/Main.js
@@ -5,13 +5,32 @@ class Main extends Component {
   constructor() {
     super();
     this.state = {
-      main_text: "",
+      ori_text: "",
+      top_words: [],
+      temp_top: [
+        {
+          id: "control_value",
+          count: 0,
+        },
+      ],
+      set_words: "",
+      set_chars: "",
+      set_parags: 3,
+      set_lorem: 0,
     };
   }
 
   componentDidMount() {
+    this.callApi();
+  }
+
+  callApi = () => {
     fetch(
-      "https://baconipsum.com/api/?type=meat-and-filler&paras=1&format=json",
+      "https://baconipsum.com/api/?type=meat-and-filler&paras=" +
+        this.state.set_parags +
+        "&start-with-lorem=" +
+        this.state.set_lorem +
+        "&format=json",
       {
         method: "GET",
       }
@@ -21,15 +40,88 @@ class Main extends Component {
       })
       .then((data) => {
         console.log(data);
-        if (data[0] !== undefined) this.setState({ main_text: data[0] });
+        let temp_text = " ";
+        if (data[0] !== undefined)
+          for (let i = 0; i < data.length; i++) temp_text = temp_text + data[i];
+
+        this.setState({ ori_text: temp_text }, this.splitWords);
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
+
+  splitWords = () => {
+    //normalize text, removing puntuation, line breaks and lower casing the whole string
+    let temp_text = this.state.ori_text
+      .toLowerCase()
+      .replace(/[.,#!$%&;:{}=`~()]/g, " ");
+
+    let temp_words = temp_text.split(" ").filter((x) => x !== ""); //split string into words (array)
+
+    //counting parags words and chars
+    this.setState({
+      set_words: temp_words.length,
+      set_chars: this.state.ori_text.length,
+    });
+
+    for (let i = 0; i < temp_words.length; i++) {
+      if (this.state.temp_top.findIndex((x) => x.id === temp_words[i]) > -1)
+        continue;
+      let temp_count = temp_text.split(" " + temp_words[i] + " ").length - 1;
+      this.setState({
+        temp_top: this.state.temp_top.push({
+          id: temp_words[i],
+          count: temp_count,
+        }),
+      });
+    }
+    this.setState({
+      top_words: this.state.temp_top.sort((a, b) =>
+        a.count > b.count ? -1 : a.count < b.count ? 1 : 0
+      ),
+    });
+  };
 
   render() {
-    return <div>{"Text: " + this.state.main_text}</div>;
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+          justifyContent: "space-around",
+        }}
+      >
+        <div>{"Text: " + this.state.ori_text}</div>
+        <div>{"TOP WORDS:"}</div>
+        {this.state.top_words[0] !== undefined ? (
+          <div>
+            <div>
+              {"1: " +
+                this.state.top_words[0].id +
+                " (" +
+                this.state.top_words[0].count +
+                ")"}
+            </div>
+            <div>
+              {"2: " +
+                this.state.top_words[1].id +
+                " (" +
+                this.state.top_words[1].count +
+                ")"}
+            </div>
+            <div>
+              {"3: " +
+                this.state.top_words[2].id +
+                " (" +
+                this.state.top_words[2].count +
+                ")"}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 
