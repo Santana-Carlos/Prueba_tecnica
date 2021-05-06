2722f67789c847e2ae2e721d1bc03354e18b8812
Diego Colmenares <diego.colmenares@uao.edu.co>
2021-05-06 12:43:42 -0500

    commit 06
    Layout and visuals completion

--- prototype1/src/Components/Main/Main.js
+++ prototype1/src/Components/Main/Main.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
-import { Container, SvgIcon } from "@material-ui/core";
+import { FormControlLabel, SvgIcon } from "@material-ui/core";
 import { ReactComponent as IconChili } from "../../Assets/chili.svg";
+import {
+  StyledContainer,
+  OrangeButton,
+  StyledInput,
+  StyledTextField,
+  StyledCheckbox,
+} from "../StyledComponents";
 import {
   Chart,
   Series,
@@ -9,14 +16,15 @@ import {
   Legend,
   Tick,
   Label,
+  Margin,
   Grid,
   LoadingIndicator,
 } from "devextreme-react/chart";
 import "./Main.css";
 
 class Main extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       ori_text: "",
       top_words: [],
@@ -28,8 +36,8 @@ class Main extends Component {
       ],
       set_words: "",
       set_chars: "",
-      set_parags: 2,
-      set_lorem: 0,
+      set_parags: 1,
+      set_lorem: false,
     };
   }
 
@@ -38,11 +46,18 @@ class Main extends Component {
   }
 
   callApi = () => {
-    fetch(
-      "https://baconipsum.com/api/?type=meat-and-filler&paras=" +
+    console.log(
+      process.env.REACT_APP_API_URL +
+        "?type=meat-and-filler&paras=" +
         this.state.set_parags +
-        "&start-with-lorem=" +
-        this.state.set_lorem +
+        (this.state.set_lorem ? "&start-with-lorem=1" : "") +
+        "&format=json"
+    );
+    fetch(
+      process.env.REACT_APP_API_URL +
+        "?type=meat-and-filler&paras=" +
+        this.state.set_parags +
+        (this.state.set_lorem ? "&start-with-lorem=1" : "") +
         "&format=json",
       {
         method: "GET",
@@ -55,7 +70,8 @@ class Main extends Component {
         //console.log(data);
         let temp_text = " ";
         if (data[0] !== undefined)
-          for (let i = 0; i < data.length; i++) temp_text = temp_text + data[i];
+          for (let i = 0; i < data.length; i++)
+            temp_text = temp_text + data[i] + "\n \n";
 
         this.setState({ ori_text: temp_text }, this.splitWords);
       })
@@ -66,14 +82,15 @@ class Main extends Component {
 
   splitWords = () => {
     //normalize text, removing puntuation, line breaks and lower casing the whole string
-    let temp_text = this.state.ori_text.toLowerCase().replace(/[.,]/g, " ");
+    let temp_oritext = this.state.ori_text;
+    let temp_text = temp_oritext.toLowerCase().replace(/[.,]|\n/g, " ");
 
     let temp_words = temp_text.split(" ").filter((x) => x !== ""); //split string into words (array)
 
     //counting parags words and chars
     this.setState({
       set_words: temp_words.length,
-      set_chars: this.state.ori_text.length,
+      set_chars: temp_oritext.length,
     });
 
     for (let i = 0; i < temp_words.length; i++) {
@@ -87,11 +104,42 @@ class Main extends Component {
         }),
       });
     }
+    this.setState(
+      {
+        top_words: this.state.temp_top.sort((a, b) =>
+          a.count > b.count ? -1 : a.count < b.count ? 1 : 0
+        ),
+      },
+      this.resetWords
+    );
+  };
+
+  resetWords = () => {
     this.setState({
-      top_words: this.state.temp_top.sort((a, b) =>
-        a.count > b.count ? -1 : a.count < b.count ? 1 : 0
-      ),
+      temp_top: [
+        {
+          id: "control_value",
+          count: 0,
+        },
+      ],
     });
+  };
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const check = e.target.checked;
+
+    switch (name) {
+      case "input_set_parags":
+        this.setState({ set_parags: value });
+        break;
+      case "input_set_lorem":
+        this.setState({ set_lorem: check });
+        break;
+      default:
+        break;
+    }
   };
 
   render() {
@@ -100,84 +148,125 @@ class Main extends Component {
         ? [{ id: "", count: 0 }]
         : this.state.top_words.slice(0, 3);
 
-    const chartOptions = {
-      height: 120,
-      axisX: {
-        lineThickness: 0.2,
-        tickLength: 0,
-      },
-      axisY: {
-        includeZero: true,
-        lineThickness: 0.2,
-        tickLength: 0,
-        gridColor: "#fff",
-        maximum: data[0].count,
-        interval: data[0].count,
-      },
-      data: [
-        {
-          type: "column",
-          dataPoints: data.map((x) => {
-            return {
-              label: x.id.charAt(0).toUpperCase() + x.id.slice(1),
-              y: x.count,
-              color: "#bdbdbd",
-            };
-          }),
-        },
-      ],
-    };
-
     return (
-      <Container maxWidth="xs">
+      <StyledContainer maxWidth="md">
+        {/* Title */}
         <div className="o-title">
           {"Let's get spicy"}
           <SvgIcon
+            viewBox="0 0 520 520"
             component={IconChili}
-            viewBox="-180 60 700 300"
-            style={{ color: "#ff5f58" }}
+            style={{
+              color: "#ff5f58",
+              margin: "0.6rem 0 0 0.8rem",
+              height: "2rem",
+              width: "2rem",
+              transform: "rotate(-20deg)",
+            }}
           />
         </div>
         <div className="o-normaltext">{"Bacon Ipsum Generator"}</div>
-        <div className="o-chartcontainer">
-          <div className="o-subtitle">
-            {"TOTAL WORDS: " +
-              this.state.set_words +
-              " - TOTAL CHARACTERS: " +
-              this.state.set_chars}
-          </div>
-          <div className="o-subtitle">{"HISTOGRAM TOP 3 WORDS:"}</div>
 
-          <div className="o-chart">
-            <Chart dataSource={data}>
-              <Series
-                valueField="count"
-                argumentField="id"
-                type="bar"
-                barWidth={50}
-                barPadding={0.1}
-                barGroupPadding={0.1}
-                color="#bdbdbd"
-              />
-              <ArgumentAxis>
-                <Tick visible={false} />
-                <Label indentFromAxis={1} font={{ size: 9 }} />
-              </ArgumentAxis>
-              <ValueAxis tickInterval={data[0].count}>
-                <Grid visible={false} />
-                <Tick visible={false} />
-                <Label indentFromAxis={1} font={{ size: 9 }} />
-              </ValueAxis>
-              <Legend visible={false} />
-              <LoadingIndicator
-                enabled={this.state.top_words[0] === undefined}
-              />
-            </Chart>
+        {/* Bar chart */}
+        <div className="o-chartcontainer">
+          <div className="o-row" style={{ flexWrap: "wrap" }}>
+            <div
+              className="o-subtitle o-subtitlespace"
+              style={{ marginRight: "4rem" }}
+            >
+              {"TOTAL WORDS: " + this.state.set_words}
+            </div>
+            <div className="o-subtitle o-subtitlespace">
+              {"TOTAL CHARACTERS: " + this.state.set_chars}
+            </div>
           </div>
+          <div className="o-subtitle o-subtitlespace">
+            {"HISTOGRAM TOP 3 WORDS:"}
+          </div>
+
+          <Chart className="o-chart" dataSource={data}>
+            <LoadingIndicator
+              enabled={true}
+              font={{ color: "#6f6f6f", size: 25 }}
+            />
+            <Series
+              valueField="count"
+              argumentField="id"
+              type="bar"
+              color="#bdbdbd"
+            />
+            <ArgumentAxis color="#e9e9e9" width={1}>
+              <Tick visible={false} />
+              <Label indentFromAxis={1} font={{ size: 16 }} />
+            </ArgumentAxis>
+            <ValueAxis tickInterval={data[0].count} color="#e9e9e9" width={1}>
+              <Grid visible={false} />
+              <Tick visible={false} />
+              <Label indentFromAxis={2} font={{ size: 16 }} />
+            </ValueAxis>
+            <Margin top={20} left={20} right={20} bottom={10} />
+            <Legend visible={false} />
+          </Chart>
         </div>
 
-        <div className="o-normaltext">{"Text: " + this.state.ori_text}</div>
-      </Container>
+        {/* Options and textfield */}
+        <div className="o-ipsumcontainer">
+          <div className="o-toolscontainer o-subtitle">
+            <div className="o-row" style={{ margin: "0.6rem 2rem 0.6rem 0" }}>
+              {"# PARAGRAPHS: "}
+              <StyledTextField
+                type="number"
+                name="input_set_parags"
+                value={this.state.set_parags || ""}
+                onChange={this.handleChange}
+                variant="outlined"
+                size="small"
+              />
+            </div>
+            <FormControlLabel
+              style={{ margin: "0.6rem 2rem 0.6rem 0" }}
+              control={
+                <StyledCheckbox
+                  checked={this.state.set_lorem || false}
+                  color="primary"
+                  name="input_set_lorem"
+                  onChange={this.handleChange}
+                />
+              }
+              label={<p className="o-subtitle">{"STARTS WITH LOREM"}</p>}
+              labelPlacement="start"
+            />
+            <div className="o-orangebutton">
+              <OrangeButton
+                onClick={() => {
+                  this.callApi();
+                }}
+              >
+                {"GENERATE!"}
+              </OrangeButton>
+            </div>
+          </div>
+          <StyledInput
+            multiline={true}
+            readOnly={true}
+            value={this.state.ori_text.slice(1)}
+            rows={7}
+            variant="outlined"
+            fullWidth={true}
+          />
+        </div>
+
+        <div className="o-credits">
+          {"Bacon Ipsum Consumer v" +
+            process.env.REACT_APP_VERSION +
+            " 2021 - " +
+            "Diego Andrés Colmenares"}
+          <br />
+          {
+            "Other Credits: Icon made by FreePik from www.flaticon.com, BarChart from js.devexpress.com"
+          }
+        </div>
+      </StyledContainer>
     );
   }
 }
---
+++ prototype1/src/Components/StyledComponents/index.js
@@ -0,0 +1,74 @@
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Container,
+  Button,
+  OutlinedInput,
+  TextField,
+  Checkbox,
+} from "@material-ui/core";
+
+export const OrangeButton = withStyles((theme) => ({
+  root: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    color: "#FFFFFF",
+    fontSize: "1.1rem",
+    margin: 0,
+    fontWeight: 400,
+    textAlign: "center",
+    backgroundColor: "#ff5f58",
+    "&:hover": {
+      backgroundColor: "#d8352d",
+    },
+  },
+}))(Button);
+
+export const StyledCheckbox = withStyles((theme) => ({
+  colorPrimary: {
+    padding: "0 0 0 0.5rem",
+    "&.Mui-checked": {
+      color: "#ff5f58",
+    },
+  },
+}))(Checkbox);
+
+export const StyledTextField = withStyles((theme) => ({
+  root: {
+    margin: "0 0 0 0.5rem",
+    padding: 0,
+    maxWidth: "3.5rem",
+    textAlign: "right",
+    backgroundColor: "#fff",
+    "& .MuiOutlinedInput-root": {
+      "&.Mui-focused fieldset": {
+        borderColor: "#ff5f58",
+      },
+    },
+  },
+}))(TextField);
+
+export const StyledInput = withStyles((theme) => ({
+  root: {
+    fontSize: "1.05rem",
+    fontWeight: "500",
+    lineHeight: "2rem",
+    padding: "1.5rem 0",
+    borderRadius: "0 0 5px 5px",
+    color: "#4f4f4f",
+    backgroundColor: "#fff",
+  },
+  notchedOutline: {
+    border: "none",
+  },
+  input: {
+    padding: "0 1.1rem",
+  },
+}))(OutlinedInput);
+
+export const StyledContainer = withStyles((theme) => ({
+  maxWidthMd: {
+    maxWidth: "46rem",
+    minHeight: "100%",
+  },
+}))(Container);
